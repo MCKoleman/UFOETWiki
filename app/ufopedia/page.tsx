41c156c67a9e841253671bcb7e9abdc921de8f91
MCKoleman <manu25025@gmail.com>
2024-04-02 17:09:06 -0400

    Created UFOpedia

--- app/ufopedia/page.tsx
+++ app/ufopedia/page.tsx
@@ -26,53 +26,56 @@ export default function UFOpediaPage() {
 	}
 
 	return (
-		<div className="flex size-full flex-col items-center justify-center">
-			<div className="text-lg font-bold">UFOpedia</div>
-			<div className="flex flex-col">
-				{getCurrentData().map((elem) => (
-					<div
-						key={elem.research_id}
-						className="flex flex-col items-center justify-center p-2"
-					>
-						<div className="flex flex-row items-center gap-1">
-							<div className="font-medium">{elem.research_name}</div>
-							<div className="text-sm opacity-75">({elem.research_id})</div>
-						</div>
-						<div>{elem.notes}</div>
-						<div className="flex flex-col items-center justify-center gap-1">
-							<div className="font-medium">Prerequisites:</div>
-							<div className="flew-row flex items-center justify-center gap-2">
-								{(elem.prereqs.length > 0 &&
-									elem.prereqs?.map((req) => (
-										<Button key={`${elem.research_id}needs${req}`}>
-											{
-												getCurrentData().find(
-													(data) => data.research_id === req,
-												)?.research_name
-											}
-										</Button>
-									))) ??
-									"-"}
+		<div className="flex size-full items-center justify-center">
+			<div className="flex max-w-[1000px] flex-col items-center justify-center">
+				<div className="grid w-full grid-cols-3 justify-center text-center">
+					<div className="grid-cols-1 text-lg font-bold">Prereqs</div>
+					<div className="grid-cols-1 text-lg font-bold">UFOpedia</div>
+					<div className="grid-cols-1 text-lg font-bold">Req For</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					{getCurrentData().map((elem) => (
+						<div
+							key={elem.research_id}
+							className="grid grid-cols-3 items-center justify-center rounded-xl border-2 border-foreground-muted bg-background p-2"
+						>
+							<div className="flex grid-cols-1 flex-col items-center justify-start gap-1">
+								<div className="flex flex-col items-center justify-center gap-2">
+									{(elem.prereqs.length > 0 &&
+										elem.prereqs?.map((req) => (
+											<Button key={`${elem.research_id}needs${req}`}>
+												{
+													getCurrentData().find(
+														(data) => data.research_id === req,
+													)?.research_name
+												}
+											</Button>
+										))) ?? <div className="opacity-75">None</div>}
+								</div>
+							</div>
+							<div className="flex grid-cols-1 flex-col items-center justify-start">
+								<div className="flex flex-row items-center gap-1 font-medium">
+									{elem.research_name}
+								</div>
+								<div>{elem.notes}</div>
+							</div>
+							<div className="flex grid-cols-1 flex-col items-center justify-start gap-1">
+								<div className="flex flex-col items-center justify-center gap-2">
+									{(elem.req_for.length > 0 &&
+										elem.req_for?.map((unlock) => (
+											<Button key={`${elem.research_id}unlocks${unlock}`}>
+												{
+													getCurrentData().find(
+														(data) => data.research_id === unlock,
+													)?.research_name
+												}
+											</Button>
+										))) ?? <div className="opacity-75">None</div>}
+								</div>
 							</div>
 						</div>
-						<div className="flex flex-col items-center justify-center gap-1">
-							<div className="font-medium">Prerequisite For:</div>
-							<div className="flex flex-row items-center justify-center gap-2">
-								{(elem.req_for.length > 0 &&
-									elem.req_for?.map((unlock) => (
-										<Button key={`${elem.research_id}unlocks${unlock}`}>
-											{
-												getCurrentData().find(
-													(data) => data.research_id === unlock,
-												)?.research_name
-											}
-										</Button>
-									))) ??
-									"-"}
-							</div>
-						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
 	)
