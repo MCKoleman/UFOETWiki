e94d9cf621eeec64cdf01ed446cc7ffbd096af66
MCKoleman <manu25025@gmail.com>
2024-04-12 10:49:07 -0400

    Added jetpack research

--- data/ufopedia-gold.ts
+++ data/ufopedia-gold.ts
@@ -2,4 +2,21 @@ import { ResearchEntry } from "@/types/research-entry"
 
 import { VANILLA_UFOPEDIA } from "./ufopedia-vanilla"
 
-export const GOLD_UFOPEDIA: Array<ResearchEntry> = [...VANILLA_UFOPEDIA]
+export const GOLD_UFOPEDIA: Array<ResearchEntry> = [
+	...VANILLA_UFOPEDIA,
+	{
+		researchID: "personal_jet_pack",
+		researchName: "Personal Jet Pack",
+		researchType: "Armor",
+		description: "",
+		prereqs: ["sentinel"],
+		reqFor: [],
+		type: "Armor",
+		mechanicalProt: 0,
+		chemicalProt: 0,
+		fireProt: 0,
+		price: 0,
+		difficulty: "Easy",
+		image: "personal_jetpack",
+	},
+]
